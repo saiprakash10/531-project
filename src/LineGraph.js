c6f9a44d5be08880be7fb725c0ff8d8e142632e7
Sai Prakash <saiprakash.ravichandran@gmail.com>
2023-11-23 20:37:25 -0700

    Tested data visualization using DBpedia

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,39 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Line } from 'react-chartjs-2';
 import 'chart.js/auto';
+import axios from 'axios';
 
-const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-      {
-        label: 'My First dataset',
-        fill: false,
-        lineTension: 0.1,
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: 'rgba(75,192,192,1)',
-        pointBackgroundColor: '#fff',
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: [65, 59, 80, 81, 56, 55, 40]
-      }
-    ]
-  };
+const LineGraph = () => {
+  const [graphData, setGraphData] = useState({
+    labels: [],
+    datasets: [{
+      label: 'Dataset from DBpedia',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: []
+    }]
+  });
 
-const LineGraph = () => (
-  <div className="graph-container">
-    <h3>Line Graph</h3>
-    <Line data={data} />
-  </div>
-);
+  useEffect(() => {
+    const fetchData = async () => {
+      // Dummy data for the sake of example
+      const labels = ['2010', '2011', '2012', '2013', '2014', '2015'];
+      const data = [8175133, 8272948, 8346693, 8396091, 8433806, 8463049];
+  
+      setGraphData(prevGraphData => ({
+        ...prevGraphData,
+        labels: labels,
+        datasets: [{
+          ...prevGraphData.datasets[0],
+          data: data
+        }]
+      }));
+    };
+  
+    fetchData();
+  
+    // Remove graphData from the dependencies array to prevent re-fetching on every render
+  }, []);
+  
+
+  return (
+    <div className="graph-container">
+      <h3>Line Graph</h3>
+      <Line data={graphData} />
+    </div>
+  );
+};
 
 export default LineGraph;
--- src/PieChart.js
+++ src/PieChart.js
@@ -1,20 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Pie } from 'react-chartjs-2';
+import 'chart.js/auto';
+import axios from 'axios';
 
-const data = {
-  labels: ['Red', 'Green', 'Yellow'],
-  datasets: [{
-    data: [300, 50, 100],
-    backgroundColor: ['#750000', '#36A2EB', '#FFCE56'],
-    hoverBackgroundColor: ['#750000', '#36A2EB', '#FFCE56']
-  }]
+const PieChart = () => {
+  const [graphData, setGraphData] = useState({
+    labels: [],
+    datasets: [{
+      data: [],
+      backgroundColor: [
+        '#FF6384',
+        '#36A2EB',
+        '#FFCE56'
+      ],
+      hoverBackgroundColor: [
+        '#FF6384',
+        '#36A2EB',
+        '#FFCE56'
+      ]
+    }]
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const query = `
+        PREFIX dbo: <http://dbpedia.org/ontology/>
+        PREFIX dbr: <http://dbpedia.org/resource/>
+
+        SELECT ?city ?population WHERE {
+          VALUES ?city { dbr:New_York_City dbr:Los_Angeles dbr:Chicago }
+          ?city dbo:populationTotal ?population .
+        }
+      `;
+      const url = `http://dbpedia.org/sparql?query=${encodeURIComponent(query)}&format=json`;
+
+      try {
+        const response = await axios.get(url);
+        const fetchedData = response.data.results.bindings;
+
+        const labels = fetchedData.map(item => item.city.value.split('/').pop());
+        const data = fetchedData.map(item => Number(item.population.value));
+
+        setGraphData({
+          labels: labels,
+          datasets: [{
+            ...graphData.datasets[0],
+            data: data
+          }]
+        });
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="graph-container">
+      <h3>Pie Chart - City Populations</h3>
+      <Pie data={graphData} />
+    </div>
+  );
 };
-
-const PieChart = () => (
-  <div className="graph-container">
-    <h2>Pie Chart</h2>
-    <Pie data={data} />
-  </div>
-);
 
 export default PieChart;
